c09d0a9a8fa62ed27c274bc02ecd5cb61572e636
Mac Mini <macmini@macs-mac-mini.local>
2017-11-08 18:24:39 -0600

    Initial Commit

---
+++ StartWarsAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StartWarsAPI
+//
+//  Created by Mac Mini on 11/8/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StartWarsAPI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StartWarsAPI
+//
+//  Created by Mac Mini on 11/8/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
